d38c39c2b8e7a64b8d3d84e697bbaefde94d1d0d
Sadeghi85 <sadeghi85@hotmail.com.com>
2015-08-23 19:53:05 +0430

    .

--- app/config/eShia.php
+++ app/config/eShia.php
@@ -2,8 +2,10 @@
 
 return array(
 
-	'FF0000' => 'Titr',
+	'ff0000' => 'Titr',
 	'008000' => 'Ravayat',
 	'000080' => 'Sayer',
-
+	'ffff00' => 'Ostad',
+	'985b49' => 'Imam',	
+	'0000ff' => 'Mabna',
 );
