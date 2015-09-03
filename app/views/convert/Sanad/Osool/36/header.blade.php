3cbdde0b0a3e8110682f9f636a07c432eae58580
Sadeghi85 <sadeghi85@hotmail.com.com>
2015-09-03 20:44:44 +0430

    .

---
+++ app/views/convert/Sanad/Osool/36/header.blade.php
@@ -0,0 +1,4 @@
+<p class="LessonTitle">الأستاذ الشيخ محمد السند</p>
+<p class="LessonTitle">بحث الأصول</p>
+<p class="LessonTitle" dir="ltr">{{ $year }}/{{ $month }}/{{ $day }}</p>
+<p class="LessonTitle">بسم الله الرحمن الرحيم</p>
