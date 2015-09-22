2cf6e02c0374c81184e6eb7fe9af7fda13555337
Sadeghi85 <sadeghi85@hotmail.com.com>
2015-09-22 11:16:03 +0330

    .

---
+++ app/views/convert/Tavakol/Feqh/94/header.blade.php
@@ -0,0 +1,5 @@
+﻿<p class="LessonTitle">الأستاذ السيد رحيم التوکل</p>
+<p class="LessonTitle">بحث الفقه</p>
+<p class="LessonTitle" dir="ltr">{{ $year }}/{{ $month }}/{{ $day }}</p>
+<p class="LessonTitle">بسم الله الرحمن الرحیم</p>
+
