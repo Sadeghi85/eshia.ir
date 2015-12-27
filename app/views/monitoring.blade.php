d898a49c7f947cc8fdb13e4e1c913505e81c0390
Aryan <mahdi.aryan@gmail.com>
2015-12-27 13:19:01 +0330

    ترتیب کرن هدر جدول مانیتورینگ و متن بالای جدول مانیتورینگ

--- app/views/monitoring.blade.php
+++ app/views/monitoring.blade.php
@@ -3,8 +3,9 @@
 
 <div class="wrap">
 
-
-دروس به روز شده در تاریخ
+@foreach ($results as $key => $result)
+دروس به‌روز شده امروز {{ $result['date'] }}
+@endforeach
 
 <table id="table2" width="100%" border="0"  cellpadding="0" cellspacing="0">
 
@@ -13,12 +14,12 @@
 	
 		<table class="downloadtable" width="100%" border="0" cellspacing="1" cellpadding="0">
 			<tr>
-				<td   align="center" valign="middle" class="download-td-title" >نام فایل</td>
-				<td   align="center" valign="middle" class="download-td-title" >تاریخ</td>
 				<td   align="center" valign="middle" class="download-td-title">ساعت</td>
 				<td   align="center" valign="middle" class="download-td-title">استاد</td>
 				<td   align="center" valign="middle" class="download-td-title">درس</td>
+				<td   align="center" valign="middle" class="download-td-title" >تاریخ</td>
 				<td   align="center" valign="middle" class="download-td-title">محتوا</td>
+				<td   align="center" valign="middle" class="download-td-title" >نام فایل</td>
 				<td   align="center" valign="middle" class="download-td-title">سال</td>
 				<td   align="center" valign="middle" class="download-td-title">حجم فایل</td>
 				
@@ -29,9 +30,9 @@
 				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['time'] }}</td>
 				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['teacher'] }}</td>
 				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['course'] }}</td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['date'] }}</td>
 				<td class="{{ $result['extension'] == 'wma' ? 'download-td-voice' : 'download-td-text' }}" align="center" valign="middle">{{ $result['type'] }}</td>
 				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle"><a href="{{ $result['fileUrl'] }}" target=_blank>{{ $result['file_name'] }}</a></td>
-				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['date'] }}</td>
 				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle"><a href="{{ $result['indexUrl'] }}" target=_blank>{{ $result['year'] }}</a></td>
 				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['file_size'] }}</td>
 			</tr>
