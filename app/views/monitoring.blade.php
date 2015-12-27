2874f82090382481f5beda9f9c2f0b8dd697a2a1
BaharSound <info@baharsound.com>
2015-12-27 10:59:27 +0330

    تغییر در جدول نمایش مونیتورینگ

--- app/views/monitoring.blade.php
+++ app/views/monitoring.blade.php
@@ -26,14 +26,14 @@
 			
 		@foreach ($results as $key => $result)
 			<tr>
-				<td class="<?php echo $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' ?>" align="center" valign="middle">{{ $result['file_name'] }}</td>
-				<td class="<?php echo $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' ?>" align="center" valign="middle">{{ $result['date'] }}</td>
-				<td class="<?php echo $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' ?>" align="center" valign="middle">{{ $result['time'] }}</td>
-				<td class="<?php echo $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' ?>" align="center" valign="middle">{{ $result['teacher'] }}</td>
-				<td class="<?php echo $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' ?>" align="center" valign="middle">{{ $result['course'] }}</td>
-				<td class="<?php echo $result['type'] == 'صوت' ? 'download-td-voice' : 'download-td-text' ?>" align="center" valign="middle">{{ $result['type'] }}</td>
-				<td class="<?php echo $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' ?>" align="center" valign="middle">{{ $result['year'] }}</td>
-				<td class="<?php echo $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' ?>" align="center" valign="middle">{{ $result['file_size'] }}</td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle"><a href="{{ $result['fileUrl'] }}" target=_blank>{{ $result['file_name'] }}</a></td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['date'] }}</td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['time'] }}</td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['teacher'] }}</td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['course'] }}</td>
+				<td class="{{ $result['extension'] == 'wma' ? 'download-td-voice' : 'download-td-text' }}" align="center" valign="middle">{{ $result['type'] }}</td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle"><a href="{{ $result['indexUrl'] }}" target=_blank>{{ $result['year'] }}</a></td>
+				<td class="{{ $result['year'] < 50 ? 'download-td-arabic' : 'download-td-persian' }}" align="center" valign="middle">{{ $result['file_size'] }}</td>
 			</tr>
 			
         @endforeach
