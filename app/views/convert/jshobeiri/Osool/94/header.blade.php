e5318d9d9261a86069b614ec40646c7e1d2d35be
BaharSound <info@baharsound.com>
2016-01-14 10:31:22 +0330

    صفحات کانورت اساتید (گنجی، قایینی و سیدجواد شبیری) درست شد

---
+++ app/views/convert/jshobeiri/Osool/94/header.blade.php
@@ -0,0 +1,4 @@
+<p class="LessonTitle">درس خارج اصول استاد سیدجواد شبیری</p>
+<p class="LessonTitle" dir="ltr">{{ $year }}/{{ $month }}/{{ $day }}</p>
+<p class="LessonTitle">بسم الله الرحمن الرحیم</p>
+
