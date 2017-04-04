cb18777f822283a670af034675265a82ba6dc84d
kamyarqalami <kamyarqalami@yahoo.com>
2017-04-04 16:45:07 +0430

    اصلاح منوهای سمت راست عربی و فارسی

--- app/views/partials/menu-ar.blade.php
+++ app/views/partials/menu-ar.blade.php
@@ -1,61 +1,54 @@
-﻿<a class="vmenu-title" href="/Ar/feqh/">مدرسة الفقاهة</a>
-<a class="linknolink" style="color:#006699;font-weight:bold !important;text-indent:1.5em !important;" >بالعربية</a>
-<a href="/Ar/Feqh/Archive/" style="font-size:0.7em; text-indent:1.5em !important;">النجف الأشرف </a>
-<a href="/Ar/Feqh/Archive/qaravieh/" style="font-size:0.7em; text-indent:1.5em !important;">النجف الأشرف(العتبة العلوية)</a>
-<a href="/Ar/Feqh/Archive/almasajed/" style="font-size:0.7em; text-indent:1.5em !important;">النجف الأشرف(المساجد)</a>
-<div class="khat"></div>
-<a href="/Ar/Feqh/Archive/Qom/" style="font-size:0.7em; text-indent:1.5em !important;">قم المقدسة</a>
-<a href="/Ar/Feqh/Archive/Karbala/" style="font-size:0.7em; text-indent:1.5em !important;">کربلاء المقدسة</a>
+﻿<a class="vmenu-title">لنجف الأشرف </a>
+<a href="/Ar/Feqh/Archive/" style="font-size:0.7em; text-indent:1.5em !important;">نجف</a>
+<a href="/Ar/Feqh/Archive/qaravieh/" style="font-size:0.7em; text-indent:1.5em !important;white-space: nowrap;">العتبة العلوية</a>
+<a href="/Ar/Feqh/Archive/almasajed/" style="font-size:0.7em; text-indent:1.5em !important;">المساجد</a>
+<a class="vmenu-title">قم المقدسة</a>
+<a href="/feqh/archive/" style="font-size:0.7em; text-indent:1.5em !important;">المسجد الأعظم</a>
+<a href="/Feqh/Archive/qom/" style="font-size:0.7em; text-indent:1.5em !important;">قم المقدسة</a>
+<a href="/Feqh/Archive/qom/feyzieh/" style="font-size:0.7em; text-indent:1.5em !important;">مدرسة الفيضية</a>
+<a href="/Feqh/Archive/qom/khan/" style="font-size:0.7em; text-indent:1.5em !important;">مدرسة الخان</a>
+<a href="/Feqh/Archive/qomschools/" style="font-size:0.7em; text-indent:1.5em !important;">سائر المدارس</a>
+<a href="/Ar/Feqh/Archive/Qom/" style="font-size:0.7em; text-indent:1.5em !important;">قم المقدسة - بالعربیة</a>
+<a class="vmenu-title">مشهد المقدسة</a>
+<a href="/Feqh/Archive/Mashhad/" style="font-size:0.7em; text-indent:1.5em !important;">مشهد المقدسة</a>
+<a class="vmenu-title">حوزات أخری</a>
+<a href="/Feqh/Archive/Iran/" style="font-size:0.7em; text-indent:1.5em !important;">مدن أخرى</a>
 <a href="/Ar/Feqh/Archive/lebanon/" style="font-size:0.7em; text-indent:1.5em !important;">بیروت</a>
 <a href="/Ar/Feqh/Archive/Damascus/" style="font-size:0.7em; text-indent:1.5em !important;">دمشق</a>
 <a href="/Ar/Feqh/Archive/qatif/" style="font-size:0.7em; text-indent:1.5em !important;">القطیف و الأحساء</a>
-<hr class="Separator"/>
-<a class="linknolink" style="color:#006699;font-weight:bold !important;text-indent:1.5em !important;">بالفارسية</a>
-<a href="/feqh/archive/" style="font-size:0.7em; text-indent:1.5em !important;">قم المقدسة - المسجد الأعظم</a>
-<a href="/feqh/archive/qom/" style="font-size:0.7em; text-indent:1.5em !important;">قم المقدسة</a>
-<a href="/Feqh/Archive/Qom/Feyzieh/" style="font-size:0.7em; text-indent:1.5em !important;">مدرسة الفيضية</a>						
-<a href="/Feqh/Archive/Qom/khan/" style="font-size:0.7em; text-indent:1.5em !important;">مدرسة الخان</a>
-<a href="/Ar/Feqh/Archive/qomschools/" style="font-size:0.7em; text-indent:1.5em !important;"> قم المقدسة - سائرالمدارس</a>
-<div class="khat"></div>	
-<a href="/Feqh/Archive/Mashhad/" style="font-size:0.7em; text-indent:1.5em !important;">مشهد المقدسة</a>
-<div class="khat"></div>
-<a href="/Feqh/Archive/Afghanestan/" style="font-size:0.7em; text-indent:1.5em !important;">افغانستان</a>
-<a href="/Feqh/Archive/Iran/" style="font-size:0.7em; text-indent:1.5em !important;">مدن أخرى</a>
-<a href="/Feqh/Archive/Moaser/" style="font-size:0.7em; text-indent:1.5em !important;">العلماء المعاصرين</a>
-<a href="/Feqh/Archive/Subject/" style="font-size:0.7em; text-indent:1.5em !important;">الفقه الموضوعي</a>
-<div class="khat"></div>
+<a href="/Ar/Feqh/Archive/Karbala/" style="font-size:0.7em; text-indent:1.5em !important;">کربلاء المقدسة</a>
+<a href="/Feqh/Archive/Afghanestan/" style="font-size:0.7em; text-indent:1.5em !important;">کابل</a>
+<a class="vmenu-title">موضوعات أخری</a>
 <a href="/Aqaed/Archive/" style="font-size:0.7em; text-indent:1.5em !important;">الکلام و الفلسفة</a>
-<a href="/Feqh/Archive/qom/tafsir/" style="font-size:0.7em; text-indent:1.5em !important;">التفسیر - قم المقدسة</a>
-<a href="/Feqh/Archive/mashhad/tafsir/" style="font-size:0.7em; text-indent:1.5em !important;">التفسیر - مشهد المقدسة</a>
+<a href="/Feqh/Archive/Sayeredoroos/" style="font-size:0.7em; text-indent:1.5em !important;">الطب فی الروایات</a>
+<a href="/Feqh/Archive/Subject/" style="font-size:0.7em; text-indent:1.5em !important;">الفقه الموضوعي</a> 
+<a href="/Feqh/Archive/Moaser/" style="font-size:0.7em; text-indent:1.5em !important;">العلماء المعاصرین</a>
+<a href="/feqh/Archive/qom/tafsir" style="font-size:0.7em; text-indent:1.5em !important;">التفسیر - قم المقدسة</a>
+<a href="/feqh/Archive/mashhad/tafsir" style="font-size:0.7em; text-indent:1.5em !important;">التفسیر - مشهد المقدسة</a>
 <a href="/Feqh/Archive/Rejal/" style="font-size:0.7em; text-indent:1.5em !important;">علم الرجال</a>
 <a href="/Ar/Feqh/Archive/Translate/" style="font-size:0.7em; text-indent:1.5em !important;">تقرير الدروس باللغة العربية</a>
-<div class="khat"></div>
-<a href="/Feqh/Archive/teb/" style="font-size:0.7em; text-indent:1.5em !important;">باقي الدروس</a>
-<hr class="Separator"/>
-<a href="/Ar/Feqh/Timing" style="font-size:0.7em; text-indent:3em !important;">جدول الدروس</a>
-<hr class="Separator"/>
+<a class="vmenu-title">المواقع</a>
+<a href="http://lib.eshia.ir" style="font-size:0.7em; text-indent:1.5em !important;">مکتبة مدرسةالفقاهة</a>
+<a href="http://www.wikifeqh.ir" style="font-size:0.7em; text-indent:1.5em !important;">ويکي الفقه</a>
+<a href="http://www.wikiporsesh.ir" style="font-size:0.7em; text-indent:1.5em !important;">ويکي السؤال</a>
 <a href="http://taqrir.eshia.ir/" style="font-size:0.7em; text-indent:1.5em !important;">تقریر مدرسةالفقاهة</a>
-<a href="/Ar/Help/taqrir/" style="font-size:0.7em; text-indent:1.5em !important;">دلیل کتابة التقریر</a>
-<hr class="Separator"/>
-<a href="http://ar.lib.eshia.ir" style="font-size:0.7em; text-indent:1.5em !important;">مکتبة مدرسةالفقاهة</a>
-<a href="http://lib.efatwa.ir" style="font-size:0.7em; text-indent:1.5em !important;white-space: nowrap;">مکتبة مدرسةالفقاهة (کتب أهل السنة)</a>
-<a href="/Ar/Help/Lib/" style="font-size:0.7em; text-indent:1.5em !important;">دلیل مکتبة مدرسةالفقاهة</a>
-<hr class="Separator"/>
-<a href="http://ar.wikifeqh.ir" style="font-size:0.7em; text-indent:1.5em !important;">ويکي الفقه</a>
-<a href="http://ar.wikiporsesh.ir" style="font-size:0.7em; text-indent:1.5em !important;">ويکي السؤال</a>
+<a class="vmenu-title">السائر</a>
+<a href="/Ar/Feqh/Timing" style="font-size:0.7em;  text-indent:1.5em  !important;">جدول الدروس</a>
+<a href="/Ar/Help/taqrir/" style="font-size:0.7em;  text-indent:1.5em !important;">دلیل کتابة التقریر</a>
+<!--                                <a href="/feqh/archive/exam/92/2/" style="font-size:0.7em; text-indent:1.5em !important;">امتحانات حوزوی</a>-->
 <a href="/Ar/android/" class="vmenu-title">تطبيق اندرويد 38-37</a>
-<a href="/Ar/android/" style="font-size:0.7em; text-indent:3em !important;">حول تطبيق اندرويد</a>
-<a href="/Ar/help/Android/" style="font-size:0.7em; text-indent:3em !important;">دلیل تطبيق اندرويد</a>
+<a href="/Ar/android/" style="font-size:0.7em;  text-indent:1.5em  !important;">حول تطبيق اندرويد</a>
+<a href="/Ar/help/Android/" style="font-size:0.7em;  text-indent:1.5em  !important;">دلیل تطبيق اندرويد</a>
 <a class="vmenu-title" href="/Ar/eshia/">الأسئلة الشرعية</a>
-<a href="/Ar/eshia/Timing/" style="font-size:0.7em; text-indent:3em !important;">مواعيد الاجابة</a>
-<a href="/Ar/eshia/FAQ/" style="font-size:0.7em; text-indent:3em !important;">الأسئلة المبوبة</a>
+<a href="/Ar/eshia/Timing/" style="font-size:0.7em;  text-indent:1.5em  !important;">مواعيد الاجابة</a>
+<a href="/Ar/eshia/FAQ/" style="font-size:0.7em; text-indent:1.5em   !important;">الأسئلة المبوبة</a>
 <a class="vmenu-title" href="/Ar/UserManual/">دليل الأستعمال</a>
-<a href="/Ar/userManual/#Download/"  style="font-size:0.7em; text-indent:3em !important;">تحميل البرنامج</a>
-<a href="/Ar/userManual/#HowtoTalk/"  style="font-size:0.7em; text-indent:3em !important;">دليل المحادثة</a>
-<a href="/Ar/userManual/#Chatroom/"  style="font-size:0.7em; text-indent:3em !important;">القاعات</a>
-<a href="/Ar/userManual/#accessory/"  style="font-size:0.7em; text-indent:3em !important;">الأشياء الجانبيه</a>
-<a href="/Ar/userManual/#FAQ/"  style="font-size:0.7em; text-indent:3em !important;">الأسئلة الشائعة</a>
-<a href="/ar/userManual/languagesetting/"  style="font-size:0.7em; text-indent:3em !important;">مشكلة ظهور الحروف بالعربية</a>
+<a href="/Ar/userManual/#Download/"  style="font-size:0.7em;  text-indent:1.5em  !important;">تحميل البرنامج</a>
+<a href="/Ar/userManual/#HowtoTalk/"  style="font-size:0.7em; text-indent:1.5em   !important;">دليل المحادثة</a>
+<a href="/Ar/userManual/#Chatroom/"  style="font-size:0.7em;   text-indent:1.5em !important;">القاعات</a>
+<a href="/Ar/userManual/#accessory/"  style="font-size:0.7em;  text-indent:1.5em  !important;">الأشياء الجانبيه</a>
+<a href="/Ar/userManual/#FAQ/"  style="font-size:0.7em;  text-indent:1.5em  !important;">الأسئلة الشائعة</a>
+<a href="/ar/userManual/languagesetting/"  style="font-size:0.7em; text-indent:1.5em  !important;">مشكلة ظهور الحروف بالعربية</a>
 <a class="vmenu-title" href="/Ar/Register/Register.asp?State=NewProfile">التسجيل</a>
-<a href="/Ar/Register/Register.asp?State=NewProfile" style="font-size:0.7em; text-indent:3em !important;">التسجيل</a>
-<a href="/Ar/Register/Register.asp" style="font-size:0.7em; text-indent:3em !important;">تعديل البيانات</a>
+<a href="/Ar/Register/Register.asp?State=NewProfile" style="font-size:0.7em;  text-indent:1.5em  !important;">التسجيل</a>
+<a href="/Ar/Register/Register.asp" style="font-size:0.7em;  text-indent:1.5em  !important;">تعديل البيانات</a>
--- app/views/partials/menu-fa.blade.php
+++ app/views/partials/menu-fa.blade.php
@@ -1,67 +1,58 @@
-﻿<a class="vmenu-title" href="/feqh/">مدرسه فقاهت</a>
-<a href="/feqh/archive/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه قم-مسجد اعظم</a>
-<a href="/feqh/archive/qom/ramezan/" style="font-size:0.7em;  text-indent:1.5em !important;">دروس تابستان</a>
-<a href="/Feqh/Archive/qom/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه قم</a>
-<a href="/Feqh/Archive/qom/feyzieh/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه قم - مدرسه فیضیه</a>
-<a href="/Ar/Feqh/Archive/Qom/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه قم-به زبان عربی</a>
-<a href="/Feqh/Archive/qom/khan/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه قم - مدرسه خان</a>
-<a href="/Feqh/Archive/qomschools/" style="font-size:0.7em; text-indent:1.5em !important;">سایر مدارس قم</a>
-<div class="khat"></div>
-<a href="/Ar/Feqh/Archive/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه نجف</a>
-<a href="/Ar/Feqh/Archive/qaravieh/" style="font-size:0.7em; text-indent:1.5em !important;white-space: nowrap;">حوزه علمیه نجف-حرم امیرالمؤمنین(ع)</a>
-
-<a href="/Ar/Feqh/Archive/almasajed/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه نجف-مساجد</a>
-<div class="khat"></div>
+﻿<a class="vmenu-title">حوزه‌ علميه قم </a>
+<a href="/feqh/archive/" style="font-size:0.7em; text-indent:1.5em !important;">مسجد اعظم</a>
+<a href="/Feqh/Archive/qom/" style="font-size:0.7em; text-indent:1.5em !important;">سایر دروس</a>
+<a href="/Feqh/Archive/qom/feyzieh/" style="font-size:0.7em; text-indent:1.5em !important;">مدرسه فیضیه</a>
+<a href="/Feqh/Archive/qom/khan/" style="font-size:0.7em; text-indent:1.5em !important;">مدرسه خان</a>
+<a href="/Feqh/Archive/qomschools/" style="font-size:0.7em; text-indent:1.5em !important;">سایر مدارس</a>
+<a href="/Ar/Feqh/Archive/Qom/" style="font-size:0.7em; text-indent:1.5em !important;">دروس به زبان عربی</a>
+<a class="vmenu-title">حوزه ‌علميه نجف </a>
+<a href="/Ar/Feqh/Archive/" style="font-size:0.7em; text-indent:1.5em !important;">نجف</a>
+<a href="/Ar/Feqh/Archive/qaravieh/" style="font-size:0.7em; text-indent:1.5em !important;white-space: nowrap;">حرم امیرالمؤمنین(ع)</a>
+<a href="/Ar/Feqh/Archive/almasajed/" style="font-size:0.7em; text-indent:1.5em !important;">مساجد</a>
+<a class="vmenu-title">حوزه علميه مشهد</a>
 <a href="/Feqh/Archive/Mashhad/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه مشهد</a>
-<div class="khat"></div>
-<a href="/Ar/Feqh/Archive/Karbala/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه کربلا</a>
-<a href="/Ar/Feqh/Archive/lebanon/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه بیروت</a>
-<a href="/Ar/Feqh/Archive/Damascus/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه دمشق</a>
-<a href="/Ar/Feqh/Archive/qatif/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه قطیف و احساء </a>
-<a href="/Feqh/Archive/Afghanestan/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه علمیه افغانستان</a>
-<div class="khat"></div>
-<a href="/Feqh/Archive/Iran/" style="font-size:0.7em; text-indent:1.5em !important;">حوزه های علمیه شهرستانها</a>
-<a href="/Feqh/Archive/Moaser/" style="font-size:0.7em; text-indent:1.5em !important;">بزرگان معاصر</a>
-<a href="/Feqh/Archive/Subject/" style="font-size:0.7em; text-indent:1.5em !important;">فقه موضوعی</a> 
+<a class="vmenu-title">سایر حوزه‌ها</a>
+<a href="/Feqh/Archive/Iran/" style="font-size:0.7em; text-indent:1.5em !important;">شهرستانها</a>
+<a href="/Ar/Feqh/Archive/lebanon/" style="font-size:0.7em; text-indent:1.5em !important;">بیروت</a>
+<a href="/Ar/Feqh/Archive/Damascus/" style="font-size:0.7em; text-indent:1.5em !important;">دمشق</a>
+<a href="/Ar/Feqh/Archive/qatif/" style="font-size:0.7em; text-indent:1.5em !important;">قطیف و احساء </a>
+<a href="/Ar/Feqh/Archive/Karbala/" style="font-size:0.7em; text-indent:1.5em !important;">کربلا</a>
+<a href="/Feqh/Archive/Afghanestan/" style="font-size:0.7em; text-indent:1.5em !important;">کابل</a>
+<a class="vmenu-title">سایر موضوع‌ها</a>
 <a href="/Aqaed/Archive/" style="font-size:0.7em; text-indent:1.5em !important;">کلام و فلسفه</a>
+<a href="/Feqh/Archive/Sayeredoroos/" style="font-size:0.7em; text-indent:1.5em !important;">طب در روایات</a>
+<a href="/Feqh/Archive/Subject/" style="font-size:0.7em; text-indent:1.5em !important;">فقه موضوعی</a> 
+<a href="/Feqh/Archive/Moaser/" style="font-size:0.7em; text-indent:1.5em !important;">بزرگان معاصر</a>
 <a href="/feqh/Archive/qom/tafsir" style="font-size:0.7em; text-indent:1.5em !important;">تفسیر - قم</a>
 <a href="/feqh/Archive/mashhad/tafsir" style="font-size:0.7em; text-indent:1.5em !important;">تفسیر - مشهد</a>
 <a href="/Feqh/Archive/Rejal/" style="font-size:0.7em; text-indent:1.5em !important;">رجال</a>
-<div class="khat"></div>
-<a href="/Feqh/Archive/Sayeredoroos/" style="font-size:0.7em; text-indent:1.5em !important;">سایر دروس</a>
-<div class="khat"></div>
 <a href="/Ar/Feqh/Archive/Translate/" style="font-size:0.7em; text-indent:1.5em !important;">تقریر عربی درس‌ها</a>
-<hr class="Separator"/>
-<a href="/Feqh/Timing/" style="font-size:0.7em; text-indent:1.5em !important;">زمان درس ها</a>
-<a href="/Feqh/DVD/" style="font-size:0.7em; text-indent:1.5em !important;">لوح فشرده</a>
-<hr class="Separator"/>
-<a href="http://taqrir.eshia.ir/" style="font-size:0.7em; text-indent:1.5em !important;">تقریر مدرسه فقاهت</a>
-<a href="/Help/taqrir/" style="font-size:0.7em; text-indent:1.5em !important;">راهنمای تقریر نویسی</a>
-<hr class="Separator"/>
+<a class="vmenu-title">سایت‌های مرتبط</a>
 <a href="http://lib.eshia.ir" style="font-size:0.7em; text-indent:1.5em !important;">کتابخانه مدرسه فقاهت</a>
-<a href="http://lib.efatwa.ir" style="font-size:0.7em; text-indent:1.5em !important;white-space: nowrap;">کتابخانه مدرسه‌فقاهت (کتب اهل‌سنت)</a>
-<a href="/help/lib/" style="font-size:0.7em; text-indent:1.5em !important;"> راهنمای کتابخانه مدرسه فقاهت</a>
-<hr class="Separator"/>
 <a href="http://www.wikifeqh.ir" style="font-size:0.7em; text-indent:1.5em !important;">ویکی فقه</a>
 <a href="http://www.wikiporsesh.ir" style="font-size:0.7em; text-indent:1.5em !important;">ویکی پرسش</a>
-
+<a href="http://taqrir.eshia.ir/" style="font-size:0.7em; text-indent:1.5em !important;">تقریر مدرسه فقاهت</a>
+<a class="vmenu-title">سایر</a>
+<a href="/Feqh/Timing/" style="font-size:0.7em; text-indent:1.5em !important;">زمان درس ها</a>
+<a href="/Feqh/DVD/" style="font-size:0.7em; text-indent:1.5em !important;">لوح فشرده</a>
+<a href="/Help/taqrir/" style="font-size:0.7em; text-indent:1.5em !important;">راهنمای تقریر نویسی</a>
 <!--                                <a href="/feqh/archive/exam/92/2/" style="font-size:0.7em; text-indent:1.5em !important;">امتحانات حوزوی</a>-->
 <a href="/android/" class="vmenu-title">نسخه اندروید 96-95</a>
-<a href="/android/" style="font-size:0.7em; text-indent:3em !important;">درباره نسخه اندروید</a>
-<a href="/help/Android/" style="font-size:0.7em; text-indent:3em !important;">راهنمای نسخه اندروید</a>
+<a href="/android/" style="font-size:0.7em; text-indent:1.5em !important;">درباره نسخه اندروید</a>
+<a href="/help/Android/" style="font-size:0.7em; text-indent:1.5em !important;">راهنمای نسخه اندروید</a>
 <a href="/eshia/" class="vmenu-title">پرسشگری دینی</a>
-<a href="/eshia/Timing/" style="font-size:0.7em; text-indent:3em !important;">زمان پاسخگویی</a>
-<a href="/eshia/Mobile/" style="font-size:0.7em; text-indent:3em !important;">نسخه موبایلی</a>
+<a href="/eshia/Timing/" style="font-size:0.7em; text-indent:1.5em !important;">زمان پاسخگویی</a>
+<a href="/eshia/Mobile/" style="font-size:0.7em; text-indent:1.5em !important;">نسخه موبایلی</a>
 <a href="/userManual/" class="vmenu-title">راهنمای استفاده</a>
-<a href="/userManual/#Download" style="font-size:0.7em; text-indent:3em !important;">دریافت نرم افزار</a>
-<a href="/userManual/#HowtoTalk" style="font-size:0.7em; text-indent:3em !important;">نحوه گفتگو</a>
-<a href="/userManual/#Chatroom" style="font-size:0.7em; text-indent:3em !important;">تالارها</a>
-<a href="/userManual/#accessory" style="font-size:0.7em; text-indent:3em !important;">امکانات جانبی</a>
-<a href="/userManual/#FAQ/" style="font-size:0.7em; text-indent:3em !important;">سوالهای متداول</a>
-<a href="/userManual/languagesetting" style="font-size:0.7em; text-indent:3em !important;">مشکل نمایش حروف فارسی</a>
+<a href="/userManual/#Download" style="font-size:0.7em; text-indent:1.5em !important;">دریافت نرم افزار</a>
+<a href="/userManual/#HowtoTalk" style="font-size:0.7em; text-indent:1.5em !important;">نحوه گفتگو</a>
+<a href="/userManual/#Chatroom" style="font-size:0.7em; text-indent:1.5em !important;">تالارها</a>
+<a href="/userManual/#accessory" style="font-size:0.7em; text-indent:1.5em !important;">امکانات جانبی</a>
+<a href="/userManual/#FAQ/" style="font-size:0.7em; text-indent:1.5em !important;">سوالهای متداول</a>
+<a href="/userManual/languagesetting" style="font-size:0.7em; text-indent:1.5em !important;">مشکل نمایش حروف فارسی</a>
 
 <a href="/Register/Register.asp?State=NewProfile" class="vmenu-title">ثبت نام</a>
-<a href="/Register/Register.asp?State=NewProfile" style="font-size:0.7em; text-indent:3em !important;">ثبت نام</a>
-<a href="/Register/Register.asp" style="font-size:0.7em; text-indent:3em !important;">ویرایش اطلاعات</a>
+<a href="/Register/Register.asp?State=NewProfile" style="font-size:0.7em; text-indent:1.5em !important;">ثبت نام</a>
+<a href="/Register/Register.asp" style="font-size:0.7em; text-indent:1.5em !important;">ویرایش اطلاعات</a>
 
 <a href="http://payam.eshia.ir" class="vmenu-title">پیام رسان</a>
