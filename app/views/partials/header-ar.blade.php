40e126ba6b2e708eb55427443da8af8766d1c38c
kamyarqalami <kamyarqalami@yahoo.com>
2017-04-23 16:30:22 +0430

    اصلاح منوی عربی

--- app/views/partials/header-ar.blade.php
+++ app/views/partials/header-ar.blade.php
@@ -1,8 +1,6 @@
-<span><a href="/default.htm">فارسي</a>&zwnj;</span>
-<span><a href="/Ar/DownLoad/">تحميل البرنامج</a>&zwnj;</span>
-<span><a href="/Ar/Contact/">اتصل بنا</a>&zwnj;</span>
-<span><a href="/Ar/Support/">الدعم المعنوي</a>&zwnj;</span>
-<span><a href="/Ar/News/">جديد الموقع</a>&zwnj;</span>
-<span><a href="/Ar/Poster/">الإعلانات</a>&zwnj;</span>
-<span><a href="/Ar/feqh/report/asatid/">الإحصائیات</a>&zwnj;</span>
-<span><a href="/Ar/advanced-search">بحث متقدم</a>&zwnj;</span>
+<span><a href="/default.htm">فارسي</a>&zwnj;</span> |
+<span><a href="/Ar/DownLoad/">البرنامج</a>&zwnj;</span> |
+<span><a href="/Ar/Contact/">اتصل بنا</a>&zwnj;</span> |
+<span><a href="/Ar/feqh/report/asatid/">الإحصائیات</a>&zwnj;</span> |
+<span><a href="/Ar/feqh/timing/">المباشر</a>&zwnj;</span> |
+<span><a href="/feqh/monitoring/">درس اليوم</a>&zwnj;</span>
