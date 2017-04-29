59aa32471874ff14f39e629097e158d273be7d38
BaharSound <info@baharsound.com>
2017-04-29 09:06:21 +0430

    رجوع به لیست درس اضافه شد

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -61,13 +61,17 @@
 									<form action="#" id="mainSearchPanel" class="searchform" onsubmit="do_search(document.getElementById('search_input').value);return false;" style="">
 										<div>
 											<label for="search_input" ></label>
-											<input name="key" id="search_input" value="@lang('app.default_search')" class="SearchInput ui-autocomplete-input empty-search-item" autocomplete="off" onfocus="if (this.value == '@lang('app.default_search')') {this.value = ''; this.className='SearchInput ui-autocomplete-input'}" onblur="if (this.value == '') {this.value = '@lang('app.default_search')'; this.className='SearchInput ui-autocomplete-input empty-search-item'}" />
+											<input name="key" id="search_input" value="@lang('app.default_search')" class="SearchInput ui-autocomplete-input empty-search-item" onfocus="if (this.value == '@lang('app.default_search')') {this.value = ''; this.className='SearchInput ui-autocomplete-input'}" onblur="if (this.value == '') {this.value = '@lang('app.default_search')'; this.className='SearchInput ui-autocomplete-input empty-search-item'}" />
 											<input type="submit" id="searchButton" value="" class="SearchKey">
 										</div>
 									</form>
 								</td>
 								<td class="header-links-TDadv-search">
-									<span><a href="advanced-search">@lang('app.advanced_button')</a>&zwnj;</span>
+									@if($locale == 'ar')
+										<span><a href="/Ar/advanced-search">@lang('app.advanced_button')</a>&zwnj;</span>
+									@else
+										<span><a href="/advanced-search">@lang('app.advanced_button')</a>&zwnj;</span>
+									@endif
 								</td>
 	
 							<!--</div>-->
@@ -83,7 +87,13 @@
 				</div>
 
 				
-				<div id="content">@yield('content')</div>
+				<div id="content">
+					
+						{{ $lessonsList or '' }}
+					
+				
+						@yield('content')
+				</div>
 				
 			</td>
 
