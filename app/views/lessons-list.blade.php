73aad372691da91e6a6ad690006d85b17736720e
kamyarqalami <kamyarqalami@yahoo.com>
2017-04-30 16:22:52 +0430

    اصلاحات تایتل درس ها

--- app/views/lessons-list.blade.php
+++ app/views/lessons-list.blade.php
@@ -1,17 +1,10 @@
 @php
 	$locale = App::getLocale();
 @endphp
-<span style="    float: right;
-    margin-right: 20px;
-    margin-top: 10px;
-    position: absolute;">
+<span class="backtoleslist">
 @if($locale == 'ar')
-	<a href="/Ar/Feqh/Archive/{{ $teacher }}/{{ $lesson }}/{{ $year }}_{{ $year+1 }}#lesson_{{ $date }}" style="    color: #bf5e2e !important;
-    font-weight: bold;
-    font-size: 14px;">الرجوع إلى قائمة الدروس</a>
+	<a  href="/Ar/Feqh/Archive/{{ $teacher }}/{{ $lesson }}/{{ $year }}_{{ $year+1 }}#lesson_{{ $date }}">  < الرجوع إلى قائمة الدروس</a>
 @else
-	<a href="/Feqh/Archive/{{ $teacher }}/{{ $lesson }}/{{ $year }}#lesson_{{ $date }}" style="    color: #bf5e2e !important;
-    font-weight: bold;
-    font-size: 14px;">رجوع به لیست درس</a>
+	<a href="/Feqh/Archive/{{ $teacher }}/{{ $lesson }}/{{ $year }}#lesson_{{ $date }}"> < بازگشت  به لیست دروس </a>
 @endif
 </span>
